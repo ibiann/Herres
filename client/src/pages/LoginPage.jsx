56794a5a6d455c4498573497c3ba13611b3713cc
ibiann <trungpham152000@gmail.com>
2022-06-22 10:36:31 +0700

    updating follow

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,81 +1,123 @@
-import React from 'react';
-import {makeStyles} from "@mui/styles";
-import {Button, Input, Typography} from "antd";
-import {Box, Link, Paper} from "@mui/material";
-import logo from '../assets/img/logo.png'
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import { Button, Input, Typography } from "antd";
+import { Box, Link, Paper } from "@mui/material";
+import logo from "../assets/img/logo.png";
+import { useForm, Controller } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { loginAction } from "../redux/reducers/auth";
+import { useNavigate } from "react-router-dom";
 const useStyle = makeStyles({
-   loginFormContainer: {
-       width: '100%',
-       maxWidth: 320,
-       backgroundColor: 'red',
-       padding: 22
-   },
-    input: {
-       margin: '8px 0px'
-    },
-    loginButton: {
-       backgroundColor: '#5AAC44',
-        color: '#FFF',
-        width: '100%',
-        fontWeight: 'bold'
-    },
-    loginTitle: {
-       color: '#606E87',
-        fontSize: 16,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 12
-    },
-    logoContainer: {
-       display: 'flex',
-        alignItems: 'center',
-        marginBottom: 40
-    },
-    logoImage: {
-       marginRight: 6,
-       height: '6.1vh'
-    },
-    logoTitle: {
-       fontSize: 46,
-        fontWeight: 'bold',
-        color: '#253959'
-    },
-    loginPageContainer: {
-       display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection:'column',
-        height: '100vh'
-    }
+  loginFormContainer: {
+    width: "100%",
+    maxWidth: 320,
+    backgroundColor: "red",
+    padding: 22,
+  },
+  input: {
+    margin: "8px 0px",
+  },
+  loginButton: {
+    backgroundColor: "#5AAC44",
+    color: "#FFF",
+    width: "100%",
+    fontWeight: "bold",
+  },
+  loginTitle: {
+    color: "#606E87",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  logoContainer: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: 40,
+  },
+  logoImage: {
+    marginRight: 6,
+    height: "6.1vh",
+  },
+  logoTitle: {
+    fontSize: 46,
+    fontWeight: "bold",
+    color: "#253959",
+  },
+  loginPageContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    height: "100vh",
+  },
 });
 
-
 const LoginPage = () => {
-    const classes = useStyle();
-    return (
-        <Box className={classes.loginPageContainer}>
-            <Box className={classes.logoContainer}>
-                <img className={classes.logoImage} src={logo} alt="logo"/>
-                <Typography  className={classes.logoTitle}>Trello</Typography>
-            </Box>
-            <Paper className={classes.loginFormContainer}>
-                <Typography className={classes.loginTitle} >Login to Trello</Typography>
-                <form>
-                    <Input className={classes.input} placeholder={'Enter email'} type={'email'} />
-                    <Input className={classes.input} placeholder={'Enter password'} type={'password'} />
-                    <Button className={classes.loginButton}>
-                        <Link href="/board">
-                            Login
-                        </Link>
-                    </Button>
-                    <hr/>
-                    <Link href={'/register'}>
-                        Sign up for an account
-                    </Link>
-                </form>
-            </Paper>
-        </Box>
+  const classes = useStyle();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { control, handleSubmit } = useForm({
+    defaultValues: { email: "", password: "" },
+  });
+
+  const submitLogin = (form) => {
+    dispatch(
+      loginAction({
+        data: form,
+        cb: () => {
+          navigate("/");
+        },
+      })
     );
+  };
+
+  return (
+    <Box className={classes.loginPageContainer}>
+      <Box className={classes.logoContainer}>
+        <img className={classes.logoImage} src={TrelloLogo} alt="logo" />
+        <Typography className={classes.logoTitle}>Trello</Typography>
+      </Box>
+      <Paper className={classes.loginFormContainer}>
+        <Typography className={classes.loginTitle}>Login to Trello</Typography>
+        <form>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                className={classes.input}
+                placeholder={"Enter email"}
+                type={"email"}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                className={classes.input}
+                placeholder={"Enter password"}
+                type={"password"}
+              />
+            )}
+          />
+
+          <Button
+            onClick={handleSubmit(submitLogin)}
+            className={classes.loginButton}
+          >
+            Login
+          </Button>
+          <hr />
+          <Link href={"/register"}>Sign up for an account</Link>
+        </form>
+      </Paper>
+    </Box>
+  );
 };
 
 export default LoginPage;
